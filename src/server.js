377b2c5b43dea3df09c5cd617b824201d08ff6f0
Luke Mico <uvvhibt6qc@privaterelay.appleid.com>
2022-10-29 10:22:50 +1100

    Add router

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,14 +13,40 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+app.use('/api', router)
+
+app.get('/', (req, res, next) => {
+  //   res.send({ message: 'hello' })
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.send({ message: 'hello' })
+  res.send({ data: 2 })
 })
 
 app.post('/', (req, res) => {
   console.log(req.body)
   res.send({ message: 'ok' })
 })
+
+// app.use(log)
+
+// app.get('/data', (req, res) => {
+//   res.send({ data: [1, 2, 3] })
+// })
+
+// app.put('/data', (req, res) => {})
+
+// app.delete()
+
+// app.post('/data', (req, res) => {
+//   console.log(req.body)
+//   res.send({ ok: true })
+// })
 
 export const start = () => {
   app.listen(3000, () => {
